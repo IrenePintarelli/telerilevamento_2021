219d2a40f048d69e4440dcd57f134f1eb95c898b
IrenePintarelli <80513536+IrenePintarelli@users.noreply.github.com>
2021-03-17 12:52:13 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,8 +1,17 @@
 # My first code in R for remote sensing
 
-install.packages("raster")
+# install.packages("raster")
+
 setwd("C:/lab/")
 library(raster)
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 p224r63_2011
 plot(p224r63_2011)
+
+# colour change
+cl<-colorRampPalette(c("black","grey","light grey")) (100)
+plot(p224r63_2011,col=cl)
+
+# colour change new
+cl<-colorRampPalette(c("blue","green","yellow","red","magenta")) (100)
+plot(p224r63_2011,col=cl)
