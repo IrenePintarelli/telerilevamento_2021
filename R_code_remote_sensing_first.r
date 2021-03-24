e22472627862faef05d0f59a99daebfa5d0c3f42
IrenePintarelli <80513536+IrenePintarelli@users.noreply.github.com>
2021-03-24 12:51:02 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -13,5 +13,58 @@ cl<-colorRampPalette(c("black","grey","light grey")) (100)
 plot(p224r63_2011,col=cl)
 
 # colour change new
-cl<-colorRampPalette(c("blue","green","yellow","red","magenta")) (100)
+cl<-colorRampPalette(c("blue","green","yellow","red","magenta")) 
 plot(p224r63_2011,col=cl)
+
+# Landsat bands
+# B1: Blue
+# B2: Green
+# B3: Red
+# B4: near infrared
+# B5: medium infrared
+# B6: thermal infrared
+
+# dev.off will clean the current graph
+dev.off()
+
+# plot band 1 with a predefined colour ramp palette
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B1_sre,col=cl)
+
+# two plots on the same page
+# 1 row, 2 columns
+par(mfrow=c(1,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+# 2 row, 1 columns
+par(mfrow=c(2,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+# plot the first 4 bands of Landsat
+par(mfrow=c(4,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+# for a better visualization I change the number of rows and columns
+par(mfrow=c(2,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+# I create different colour bands
+par(mfrow=c(2,2))
+clb <- colorRampPalette(c("dark blue","blue","light blue")) (100)
+plot(p224r63_2011$B1_sre, col=clb)
+clg <- colorRampPalette(c("dark green","green","light green")) (100)
+plot(p224r63_2011$B2_sre, col=clg)
+clr <- colorRampPalette(c("dark red","red","pink")) (100)
+plot(p224r63_2011$B3_sre, col=clr)
+clnir <- colorRampPalette(c("red","orange","yellow")) (100)
+plot(p224r63_2011$B4_sre, col=clnir)
+
+
+
